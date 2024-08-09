8198314ef2b023fc75fe2f32e4347f186d666bf1
antor10655gh <creativefreelancerantor@gmail.com>
2024-08-09 23:41:56 +0600

    add some change

--- src/loader/Loader.jsx
+++ src/loader/Loader.jsx
@@ -4,7 +4,6 @@ import "./Loader.css";
 const Loader = () => {
   return (
     <div class="loader-wrapper">
-      <div class="loader"></div>
       <div class="loader-overlay">
         <div class="left-overlay"></div>
         <div class="right-overlay"></div>
