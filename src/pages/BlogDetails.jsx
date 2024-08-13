0d89274ee6139b0c0dc86b89ba72a4941a0b5a23
AM Antor <creativefreelancerantor@gmail.com>
2024-08-13 11:35:22 +0600

    add some change on blogDetails page

--- src/pages/BlogDetails.jsx
+++ src/pages/BlogDetails.jsx
@@ -103,16 +103,16 @@ const BlogDetails = () => {
                   <div class="blog-item-content">
                     <div class="post-content">
                       <span class="cat">
-                        <a href="#">{blog.category}</a>
+                        <div>{blog.category}</div>
                       </span>
                       <h3 class="post-title">
-                        <a href="#">{blog.title}</a>
+                        <div>{blog.title}</div>
                       </h3>
                     </div>
                     <div class="author-info d-flex align-items-center">
                       <div class="author-info-content">
                         <h4 class="author-info-name">
-                          <a href="#0">Yomna Doe</a>
+                          <div>AM Antor</div>
                         </h4>
                         <span class="post-date">September 17, 2022</span>
                       </div>
@@ -142,31 +142,6 @@ const BlogDetails = () => {
                       </blockquote>
                     </div>
                     <div class="post-footer">
-                      <div class="post-share">
-                        <h5>Share The Post</h5>
-                        <ul class="list-unstyled social-media">
-                          <li>
-                            <a href="#">
-                              <i class="fa fa-facebook"></i>
-                            </a>
-                          </li>
-                          <li>
-                            <a href="#">
-                              <i class="fa fa-twitter"></i>
-                            </a>
-                          </li>
-                          <li>
-                            <a href="#">
-                              <i class="fa fa-linkedin"></i>
-                            </a>
-                          </li>
-                          <li>
-                            <a href="#">
-                              <i class="fa fa-google-plus"></i>
-                            </a>
-                          </li>
-                        </ul>
-                      </div>
                       <div class="post-tags">
                         <h5>Tags</h5>
                         <ul class="tags-list list-unstyled">
@@ -181,129 +156,6 @@ const BlogDetails = () => {
                           </li>
                         </ul>
                       </div>
-                    </div>
-                    <div class="post-comments">
-                      <h3>03 Comments</h3>
-                      <div class="post-comment">
-                        <div class="row">
-                          <div class="col-sm-2">
-                            <div class="comment-image">
-                              <img
-                                src="/images/comments/1.jpg"
-                                class="img-fluid"
-                                alt=""
-                              />
-                            </div>
-                          </div>
-                          <div class="col-sm-10">
-                            <h4>Wasima Yalma</h4>
-                            <span>September 17, 2022</span>
-                            <a class="reply-btn" href="#">
-                              <i class="fa fa-reply"></i>Reply
-                            </a>
-                            <p>
-                              Lorem ipsum dolor sit amet, consectetur
-                              adipisicing elit.
-                            </p>
-                          </div>
-                        </div>
-                      </div>
-                      <div class="post-comment reply">
-                        <div class="row">
-                          <div class="col-sm-2">
-                            <div class="comment-image">
-                              <img
-                                src="/images/comments/2.jpg"
-                                class="img-fluid"
-                                alt=""
-                              />
-                            </div>
-                          </div>
-                          <div class="col-sm-10">
-                            <h4>Belin Taylor</h4>
-                            <span>September 17, 2022</span>
-                            <a class="reply-btn" href="#">
-                              <i class="fa fa-reply"></i>Reply
-                            </a>
-                            <p>
-                              Excepteur sint ocacat cupidatat non proi dent sunt
-                              in culpa qui officia deserunt.
-                            </p>
-                          </div>
-                        </div>
-                      </div>
-                      <div class="post-comment">
-                        <div class="row">
-                          <div class="col-sm-2">
-                            <div class="comment-image">
-                              <img
-                                src="/images/comments/3.jpg"
-                                class="img-fluid"
-                                alt=""
-                              />
-                            </div>
-                          </div>
-                          <div class="col-sm-10">
-                            <h4>Yassir Doe</h4>
-                            <span>September 17, 2022</span>
-                            <a class="reply-btn" href="#">
-                              <i class="fa fa-reply"></i>Reply
-                            </a>
-                            <p>
-                              Lorem ipsum dolor sit amet, consectetur
-                              adipisicing elit.
-                            </p>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="post-form">
-                      <h3>Leave a Comment</h3>
-                      <form action="#" method="post">
-                        <div class="row">
-                          <div class="col-sm-6 col-xs-12">
-                            <div class="form-group">
-                              <input
-                                type="text"
-                                class="form-control"
-                                name="name"
-                                placeholder="Name"
-                                required
-                              />
-                            </div>
-                          </div>
-                          <div class="col-sm-6 col-xs-12">
-                            <div class="form-group">
-                              <input
-                                type="email"
-                                class="form-control"
-                                name="email"
-                                placeholder="Email"
-                                required
-                              />
-                            </div>
-                          </div>
-                          <div class="col-sm-12 col-xs-12">
-                            <div class="form-group">
-                              <textarea
-                                class="form-control"
-                                name="message"
-                                placeholder="Your Comment"
-                                required
-                              ></textarea>
-                            </div>
-                          </div>
-                          <div class="col-sm-12 col-xs-12">
-                            <button
-                              class="main-btn"
-                              type="submit"
-                              name="comment"
-                            >
-                              <span>Submit Comment</span>
-                            </button>
-                          </div>
-                        </div>
-                      </form>
                     </div>
                   </div>
                 </div>
